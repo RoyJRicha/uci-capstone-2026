a54453be0098e39598902b826637060e7198b6e2
Diego Gonzalez Lopez <diegg17@uci.edu>
2026-04-10 12:06:49 -0700

    begin testing EasyOCR

--- server/ocr.py
+++ server/ocr.py
@@ -30,4 +30,47 @@ In the following test, this library is unable to correctly identify the receipts
 to Alberstsons, and it captures the zip code backwards/upside down as 21926 instead of 92612.
 It is unable to capture date and amount in all tests
 """
-testReceiptParser()
+# testReceiptParser()
+
+from PIL import Image
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import easyocr  # now testing easyocr library
+
+reader = easyocr.Reader(['en'], gpu=False)
+img = np.array(Image.open("uploads/receipt1.jpg"))
+img2 = np.array(Image.open("uploads/receipt2.jpg"))
+img3 = np.array(Image.open("uploads/receipt3.jpg"))
+img4 = np.array(Image.open("uploads/receipt4.jpg"))
+
+def testEasyOCR(npImg: np.ndarray, showBoundingBox=False):
+    results = reader.readtext(npImg)
+    imgAnnotated = npImg.copy() if showBoundingBox else None
+    for (bbox, text, confidence) in results:
+        print(f"Text: {text}, | Confidence: {confidence}")
+        if showBoundingBox:
+            topLeft = tuple(map(int, bbox[0]))  # grab corners of bbox
+            topRight = tuple(map(int, bbox[1]))
+            bottomLeft = tuple(map(int, bbox[2]))
+            bottomRight = tuple(map(int, bbox[3]))
+
+            # Draw box
+            pts = np.array([topLeft, topRight, bottomLeft, bottomRight], dtype=np.int32)
+            cv2.polylines(imgAnnotated, [pts], isClosed=True, color=[0,255,0], thickness=2)
+
+            # draw text bove box
+            cv2.putText(imgAnnotated, text, topLeft, cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.6, color=(255,0,0), thickness=2)
+    
+    if showBoundingBox:
+        plt.figure(figsize=(10, 15))
+        plt.imshow(imgAnnotated)
+        plt.axis('off')
+        plt.title("Receipt OCR - Bounding Boxes")
+        plt.show()
+    print("End test")
+
+testEasyOCR(img, True)
+testEasyOCR(img2, True)
+testEasyOCR(img3, True)
+testEasyOCR(img4, True)
